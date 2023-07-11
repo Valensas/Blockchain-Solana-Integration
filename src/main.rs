a0f96d04725a0bf62dacffd6874ee0e54c5c286a
Doğa Demirtürk <demirturkdoga@gmail.com>
2023-07-11 10:31:04 +0300

    Get Latest Block Number implemented

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use] extern crate rocket;
-use rocket::http::Status;
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use solana_client::rpc_client::RpcClient;
+use solana_sdk::commitment_config::CommitmentConfig;
+use serde_json::{Result, Value};
 
 #[get("/hello")]
 fn hello() -> &'static str {
@@ -14,56 +14,46 @@ fn greet(name: &str) -> String {
     format!("Hello {}\n", name)
 }
 
-
 #[derive(Debug, Deserialize)]
 struct GreetingRequest {
     name: String
 }
 
-#[post("/greet", data = "<request>")] //JSONu alıyor, GreetingRequest'e dönüştürüyor, sonra ismi basıyor
+#[post("/greet", data = "<request>")] // JSONu alıyor, GreetingRequest'e dönüştürüyor, sonra ismi basıyor
 fn greet_json(request: &str) -> String {
     let g: GreetingRequest = serde_json::from_str(request).unwrap();
     format!("Hello {}\n", g.name)
 }
 
-#[derive(Debug,Serialize,Deserialize)]
-struct Block{
-    height: u64,
-    hash: String
-}
-impl Block {
-    fn new() -> Self {
-        Self {
-            height: 0,
-            hash: String::from("0"),
-        }
-    }
-}
-#[get("/blocks/latest")]
-fn get_latest_block() -> String{
-    let rpc_url = "https://api.devnet.solana.com";
-    let rpc_client = RpcClient::new(rpc_url.to_string());
-    let block_height = rpc_client.get_block_height().unwrap();
-    let mut block: Block = Block::new();
-
-    block.height = block_height;
-    
-    match rpc_client.get_slot() {
-        Ok(slot) => {
-            let block_hash: Result<solana_transaction_status::EncodedConfirmedBlock, String> = match rpc_client.get_block(slot) {
-                Ok(hash) => hash.to_string(),
-                Err(_) => return String::from("Error"),
-            };
-            block.hash = block_hash;
-
-            let json= serde_json::to_string(&block).unwrap();
-            json
-        },
-        Err(_) => String::from("Error"),
-    }
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![hello, greet, greet_json, get_latest_block])
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Block {
+    height: u64,
+    hash: String
+}
+
+#[get("/blocks/latest")]
+fn get_latest_block() -> String {
+    let rpc_url = "https://api.devnet.solana.com".to_string(); // Linki ekledik
+    let rpc_client = RpcClient::new(rpc_url);
+    let commitment_config = CommitmentConfig::finalized(); //TODO: İşlenmesi bitmiş block alınsın diye eklendi
+    
+    match rpc_client.get_slot_with_commitment(commitment_config) { // Son block slotunu alıp matchledik
+        Ok(slot) => {
+            let block_str = serde_json::to_string(&rpc_client.get_block(slot).unwrap()).unwrap(); // Son blockun bilgileri stringe dönüştürüldü
+            let block_json: Value = serde_json::from_str(&block_str).unwrap(); // Son blockun bilgileri JSONa dönüştürüldü
+            let block_height: u64 = block_json["blockHeight"].to_string().parse::<u64>().unwrap(); // BlockHeight alındı
+            let block_hash = block_json["blockhash"].to_string(); // Blockhash alındı
+            let block: Block = Block { // Block objesi yaratıldı
+                height: block_height,
+                hash: block_hash
+            };
+            serde_json::to_string(&block).unwrap() // Block objesi JSON stringine dönüştürüldü ve returnlendi
+        }
+        Err(_) => String::from("Slot not found")
+    }
 }
