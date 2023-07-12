40a3eaf72ff1d33a7d357c6b7e244a7872b16d55
Doğa Demirtürk <demirturkdoga@gmail.com>
2023-07-12 11:40:34 +0300

    getLatestBlock return type change

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #[macro_use] extern crate rocket;
+
 use serde::{Deserialize, Serialize};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::commitment_config::CommitmentConfig;
-use serde_json::{Result, Value};
+use rust_decimal::prelude::*;
+use rocket::serde::json::Json;
+/*use solana_transaction_status::{
+    EncodedConfirmedBlock, EncodedTransactionWithStatusMeta, EncodedConfirmedTransactionWithStatusMeta, TransactionStatus,
+    UiConfirmedBlock, UiTransactionEncoding,
+};*/
 
 #[get("/hello")]
 fn hello() -> &'static str {
@@ -27,7 +33,7 @@ fn greet_json(request: &str) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, greet, greet_json, get_latest_block])
+    rocket::build().mount("/", routes![hello, greet, greet_json, get_latest_block, scan_block_transactions_from_slot])
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -37,23 +43,115 @@ struct Block {
 }
 
 #[get("/blocks/latest")]
-fn get_latest_block() -> String {
+fn get_latest_block() -> Result<Json<Block>, String> {
     let rpc_url = "https://api.devnet.solana.com".to_string(); // Linki ekledik
     let rpc_client = RpcClient::new(rpc_url);
     let commitment_config = CommitmentConfig::finalized(); //TODO: İşlenmesi bitmiş block alınsın diye eklendi
-    
+
     match rpc_client.get_slot_with_commitment(commitment_config) { // Son block slotunu alıp matchledik
         Ok(slot) => {
-            let block_str = serde_json::to_string(&rpc_client.get_block(slot).unwrap()).unwrap(); // Son blockun bilgileri stringe dönüştürüldü
+            let block = rpc_client.get_block(slot).unwrap();
+
+            let block: Block = Block { // Block objesi yaratıldı
+                height: block.block_height.unwrap(),
+                hash: block.blockhash
+            };
+
+            Ok(Json(block))
+            //serde_json::from_str(&block) // Block objesi JSON stringine dönüştürüldü ve returnlendi
+        }
+        Err(_) => Err(String::from("Slot not found"))
+    }
+}
+
+/************************************* DOĞA ****************************************************/
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TransactionInfo { // Onur ile ortak
+    adress: String,
+    //#[serde(with = "rust_decimal::serde::str")]
+    amount: Decimal
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Transaction {
+    from: TransactionInfo,
+    to: TransactionInfo,
+    hash: String,
+    status: String,
+    contract: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockTransactions {
+    height: u64,
+    hash: String,
+    transactions: Vec<Transaction>
+}
+
+#[get("/blocks/<slot>")]
+fn scan_block_transactions_from_slot(slot: u64) -> String {
+    let rpc_url = "https://api.devnet.solana.com".to_string(); // Solana url
+    let rpc_client = RpcClient::new(rpc_url);
+
+    //let commitment_config = CommitmentConfig::finalized(); //TODO: burada gerek var mı? kullanmadım şimdilik
+
+    match rpc_client.get_block(slot) { //Verilen slot'a göre chainden block bilgileri alınıyor 
+        Ok(block) => {
+            let block_hash = block.blockhash;
+            let block_height = block.block_height.unwrap();
+            //let transactions: Vec<EncodedTransactionWithStatusMeta> = block.transactions;
+
+            "hey".to_string()
+            //transactions.into_iter().to_string().collect::<String>()
+
+            /*let block_str = serde_json::to_string(&block).unwrap(); // Son blockun bilgileri stringe dönüştürüldü
             let block_json: Value = serde_json::from_str(&block_str).unwrap(); // Son blockun bilgileri JSONa dönüştürüldü
+
             let block_height: u64 = block_json["blockHeight"].to_string().parse::<u64>().unwrap(); // BlockHeight alındı
             let block_hash = block_json["blockhash"].to_string(); // Blockhash alındı
-            let block: Block = Block { // Block objesi yaratıldı
-                height: block_height,
-                hash: block_hash
-            };
-            serde_json::to_string(&block).unwrap() // Block objesi JSON stringine dönüştürüldü ve returnlendi
+
+            block_json["transactions"].to_string()*/
+        }
+        Err(_) => String::from("Block with given slot not found!\n")
+    }
+}
+
+/*#[get("/blocks/<block_height>", rank = 1)] // rank = 1: önce u64'e cast edebiliyor mu baksın (block height), edemezse string alsın (block hash)
+fn scan_block_transactions_from_height(block_height: u64) -> String {
+    let rpc_url = "https://api.devnet.solana.com".to_string(); // Linki ekledik
+    let rpc_client = RpcClient::new(rpc_url);
+    let commitment_config = CommitmentConfig::finalized(); //TODO: burada gerek var mı?
+    
+    "hey".to_string()
+
+    match rpc_client.get_confirmed_blocks_with_commitment(block_height, commitment_config) { // Son block slotunu alıp matchledik
+        Ok(block) => {
+            let block_str = serde_json::to_string(&block).unwrap(); // Son blockun bilgileri stringe dönüştürüldü
+            let block_json: Value = serde_json::from_str(&block_str).unwrap(); // Son blockun bilgileri JSONa dönüştürüldü
+
+            block_json.to_string()
         }
         Err(_) => String::from("Slot not found")
     }
-}
+}*/
+
+/*#[get("/blocks/<block_hash>", rank = 2)]
+fn scan_block_transactions_from_hash(block_hash: String) -> String {
+    let rpc_url = "https://api.devnet.solana.com".to_string(); // Linki ekledik
+    let rpc_client = RpcClient::new(rpc_url);
+    let commitment_config = CommitmentConfig::finalized(); //TODO: burada gerek var mı?
+    
+    "hey".to_string()
+
+    match rpc_client.get_confirmed_blocks_with_commitment(&block_hash, commitment_config) { // Son block slotunu alıp matchledik
+        Ok(block) => {
+            let block_str = serde_json::to_string(&block).unwrap(); // Son blockun bilgileri stringe dönüştürüldü
+            let block_json: Value = serde_json::from_str(&block_str).unwrap(); // Son blockun bilgileri JSONa dönüştürüldü
+
+            block_json.to_string()
+        }
+        Err(_) => String::from("Slot not found")
+    }
+}*/
+/************************************* END DOĞA ****************************************************/
